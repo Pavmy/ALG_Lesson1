21c3dd5572eb8241a6a0f80f6bdc0ba936a55512
Pavmy <pavmy@hotmail.com>
2018-09-26 20:56:04 +0300

    Обмен переменных

--- ALG_Lesson1/Program.cs
+++ ALG_Lesson1/Program.cs
@@ -4,13 +4,27 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+//Павлюков Михаил
+////3. Написать программу обмена значениями двух целочисленных переменных:
+//a.с использованием третьей переменной;
+//b. * без использования третьей переменной.
+
+
 namespace ALG_Lesson1
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello");
+            int a = 3;
+            int b = 7;
+
+            a = a + b;
+            b = b - a;
+            b = -b;
+            a = a - b;
+
+            Console.WriteLine("a = "+ a + " b = "+ b);
             Console.ReadKey();
         }
     }
